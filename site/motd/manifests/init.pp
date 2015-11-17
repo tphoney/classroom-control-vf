a482dffe03ba5927b4a3ca512d9c5f3d8d1fa979
TP Honey <tphoney@users.noreply.github.com>
2015-11-17 19:23:51 +0000

    Update init.pp

--- site/motd/manifests/init.pp
+++ site/motd/manifests/init.pp
@@ -1,6 +1,7 @@
 class motd {
-  exec { 'something':
+  exec { 'cowsay':
     command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
     creates => '/etc/motd',
+    path => '/usr/bin:/usr/local/bin',
   }
 }

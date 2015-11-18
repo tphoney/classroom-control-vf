0be8b1b3b35d799f853c2e5c06e37a1290bdcad2
TP Honey <tphoney@users.noreply.github.com>
2015-11-18 19:53:23 +0000

    Update init.pp

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -9,8 +9,8 @@ group => 'root',
 mode => '0644',
 content => template('aliases/aliases.erb'),
 }
-exec { '/usr/bin/newaliases':
-refreshonly => true,
-subscribe => File['/etc/aliases'],
-}
+#exec { '/usr/bin/newaliases':
+#refreshonly => true,
+#subscribe => File['/etc/aliases'],
+#}
 }

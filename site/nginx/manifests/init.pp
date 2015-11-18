3774528674aeb26ca6cb8d5fd69ac30f72f67bd6
TP Honey <tphoney@users.noreply.github.com>
2015-11-18 18:09:37 +0000

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,17 +1,17 @@
 class nginx {
-  $http_dir = '/var/www/'
-  $nginx_dir = '/var/www/'
+  $http_dir = '/var/www'
+  $nginx_dir = '/etc/nginx'
 
   
   package { 'nginx':
     ensure => present,
   }
   
-  file { '${nginx}/nginx.conf':
+  file { '${nginx_dir}/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
   }
-  file { '${nginx}/conf.d/default.conf':
+  file { '${nginx_dir}/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],

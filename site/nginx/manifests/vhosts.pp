b079e22d824e0e32347e6e077c2048e596499eb8
TP Honey <tphoney@users.noreply.github.com>
2015-11-19 15:21:50 +0000

    Update vhosts.pp

--- site/nginx/manifests/vhosts.pp
+++ site/nginx/manifests/vhosts.pp
@@ -1,7 +1,8 @@
 define nginx::vhosts (
   $port = '80',
-  $docroot = '${::nginx::http_dir}/vhost${title}",
+  $docroot = "${::nginx::http_dir}/vhost${title}",
   $servername = $title,
+  $files_to_serve= 'index index.html index.htm'
 ) {
   host { $title:
     ip => $::ipaddress,

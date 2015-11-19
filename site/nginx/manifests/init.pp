31e1b45a9d3cd59de9b1a5c1756e9125c15f6473
TP Honey <tphoney@users.noreply.github.com>
2015-11-19 18:07:46 +0000

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,6 +1,12 @@
 class nginx (
-  $root     = undef,
-) {
+  $package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+) inherits nginx::params {
 
   case $::osfamily {
     'redhat': {
